353a660c335e54264ab8a3d3f1c8d9d0aa5f4ff5
Lilluska01 <WIUDUC@hallgato.uniduna.hu>
2025-09-04 09:30:55 +0200

    Updated

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Üdv a Szkript órán')
+    print_hi('Üdv a Szkript órán :D')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
